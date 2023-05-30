2d5e3eee149a3a1388c519fbde3d49fedf57041c
Gabriel Gonçalves <business.gabrielgoncalves@gmail.com>
2023-05-29 23:44:54 -0300

    att1 da page login

---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="cmp-login">
+        <img src="../assets/dglogin.jpg" alt="">
+        <div class="cmp-login__wrapper cmp-login__wrapper--white">Teste</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Login"
+    }
+</script>
+
+<style lang="scss">
+    .cmp-login{ /*Componente Pai*/
+        img{
+            width: 100%;
+            height: 427px;
+            object-fit: cover;
+        }
+
+        &__wrapper{
+            background: #fff;
+        }
+
+        &__wrapper--white{
+            background: white;
+        }
+
+        &__wrapper--red{
+            background: red;
+        }
+    }
+</style>
